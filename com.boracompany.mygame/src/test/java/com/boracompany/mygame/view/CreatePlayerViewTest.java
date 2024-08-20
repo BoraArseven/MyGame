6d8914b290bfdfbc55028d8ad8affe9ffd6989ad
Bora Arseven <bora.arseven@gmail.com>
2024-08-20 15:45:55 +0200

    Added Failing test to be fixed in TDD way

--- com.boracompany.mygame/src/test/java/com/boracompany/mygame/view/CreatePlayerViewTest.java
+++ com.boracompany.mygame/src/test/java/com/boracompany/mygame/view/CreatePlayerViewTest.java
@@ -1,13 +1,13 @@
 package com.boracompany.mygame.view;
 
-
-
 import static org.junit.Assert.assertNotNull;
 
 import org.assertj.swing.annotation.GUITest;
+import org.assertj.swing.core.matcher.JButtonMatcher;
 import org.assertj.swing.core.matcher.JLabelMatcher;
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.fixture.JTextComponentFixture;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
 
 import org.junit.Test;
@@ -27,24 +27,44 @@ public class CreatePlayerViewTest extends AssertJSwingJUnitTestCase {
 		window.show(); // shows the frame to test
 	}
 
-	@Test @GUITest
+	@Test
+	@GUITest
 	public void testControlsInitialStates() {
 		assertNotNull(createPlayerView);
-	window.label(JLabelMatcher.withText("Name:")).requireEnabled();
-	window.label(JLabelMatcher.withText("Damage:")).requireEnabled();
-	window.label(JLabelMatcher.withText("Health:")).requireEnabled();
-	window.textBox("NameText").requireEnabled();
-	window.textBox("DamageText").requireEnabled();
-	window.textBox("HealthText").requireEnabled();
-	window.button("MainMenuButton").requireEnabled();
-	window.button("CreatePlayerButton").requireDisabled();
-	
-	
+		window.label(JLabelMatcher.withText("Name:")).requireEnabled();
+		window.label(JLabelMatcher.withText("Damage:")).requireEnabled();
+		window.label(JLabelMatcher.withText("Health:")).requireEnabled();
+		window.textBox("NameText").requireEnabled();
+		window.textBox("DamageText").requireEnabled();
+		window.textBox("HealthText").requireEnabled();
+		window.button("MainMenuButton").requireEnabled();
+		window.button("CreatePlayerButton").requireDisabled();
 	}
+
 	@Test
-	public void testInitialSetup() {
-
+	public void testTextFields() {
+		window.textBox("DamageText").enterText("1");
+		window.textBox("NameText").enterText("test");
+		window.textBox("HealthText").enterText("100");
+		window.button(JButtonMatcher.withText("Create")).requireEnabled();
 	}
 
-
+	
+	// Can be good parameterized test, but I still am not comfortable at writing them
+	@Test
+	public void testWhenEitherNameDamageorHealthAreBlankThenAddButtonShouldBeDisabled() {
+		JTextComponentFixture damageTextBox = window.textBox("DamageText");
+		JTextComponentFixture nameTextBox = window.textBox("NameText");
+		JTextComponentFixture healthTextBox = window.textBox("HealthText");
+		
+		damageTextBox.enterText("1");
+		nameTextBox.enterText(" ");
+		healthTextBox.enterText(" ");
+		window.button(JButtonMatcher.withText("Create")).requireDisabled();
+		
+	}
+	
+	
+	
+	
 }
