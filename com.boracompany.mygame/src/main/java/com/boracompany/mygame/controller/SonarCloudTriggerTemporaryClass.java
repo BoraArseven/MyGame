e77d90d72939f141d2c2577947d13bec03b5eaed
Bora Arseven <bora.arseven@gmail.com>
2024-12-04 14:26:31 +0100

    moved triggerer class to orm to make it escape from mutation testing

--- com.boracompany.mygame/src/main/java/com/boracompany/mygame/controller/SonarCloudTriggerTemporaryClass.java
+++
@@ -1,9 +0,0 @@
-package com.boracompany.mygame.controller;
-
-public class SonarCloudTriggerTemporaryClass {
-
-	
-	public void examplemethod() {
-		System.out.println("New Line Triggers Analysis");
-	}
-}
---
+++ com.boracompany.mygame/src/main/java/com/boracompany/mygame/orm/TemporaryClass.java
@@ -0,0 +1,9 @@
+package com.boracompany.mygame.orm;
+
+public class TemporaryClass {
+	Integer x = 1;
+
+	public void examplemethod() {
+		System.out.println("New Line Triggers Analysis" + x.toString());
+	}
+}
