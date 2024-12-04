8aa739d20b52feb1988a2c0403ba6c4b3ad6e7b5
Bora Arseven <bora.arseven@gmail.com>
2024-12-04 14:18:14 +0100

    added new code to trigger analysis

---
+++ com.boracompany.mygame/src/main/java/com/boracompany/mygame/controller/SonarCloudTriggerTemporaryClass.java
@@ -0,0 +1,9 @@
+package com.boracompany.mygame.controller;
+
+public class SonarCloudTriggerTemporaryClass {
+
+	
+	public void examplemethod() {
+		System.out.println("New Line Triggers Analysis");
+	}
+}
